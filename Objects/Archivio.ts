cb6cc007007109810d91b82283fbbfe20c7a513e
mdesury <127192048+mdesury@users.noreply.github.com>
2023-06-12 16:21:59 +0200

    1 file modified

--- Objects/Archivio.ts
+++ Objects/Archivio.ts
@@ -11,10 +11,24 @@ ricercaLibri(chiave: string) {
  return this.lista.filter(libro => {
     libro.titolo.toLowerCase().includes(chiave.toLowerCase()) ||
     libro.autore.toLowerCase().includes(chiave.toLowerCase()) ||
-    libro.posizione.toLowerCase().includes(chiave.toLowerCase()) ||
-    libro.prestito.toLowerCase().includes(chiave.toLowerCase())
+    libro.posizione.toLowerCase().includes(chiave.toLowerCase())
   });
-
 }
+  aggiungiLibri(libro: string) {
+    this.lista.push(libro);
+  }
+  
+  rimuoviLibri(titolo: string, autore: string) {
+    this.lista = this.lista.filter(libro => {
+      libro.titolo !== titolo || libro.autore !== autore)
+  });
+}
+
+
+
+
+
+
+
 
 
